8ede816912a76cb2ea45c86a48f3c4b55193906a
swamyhr <swamyrangannanayaka@gmail.com>
2026-03-01 20:40:14 +0530

    updated UserMapper class

--- api/src/main/java/com/taskmanager/api/mapper/UserMapper.java
+++ api/src/main/java/com/taskmanager/api/mapper/UserMapper.java
@@ -2,15 +2,15 @@ package com.taskmanager.api.mapper;
 
 import com.taskmanager.api.dto.UserDTO;
 import com.taskmanager.api.entity.UserEntity;
-import com.taskmanager.api.model.Users;
+import com.taskmanager.api.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel="spring")
 public interface UserMapper {
 
-    @Mapping(target="password", ignore=true)
-    UserEntity toEntity(Users user);
+    @Mapping(target = "password", ignore = true)
+    UserEntity toEntity(User user);
 
     UserDTO toUserDTO(UserEntity userEntity);
 }
