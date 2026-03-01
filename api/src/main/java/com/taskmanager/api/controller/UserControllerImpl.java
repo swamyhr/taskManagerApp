0ad54f6bf534b9234d09dd178b2def8c8c43769e
swamyhr <swamyrangannanayaka@gmail.com>
2026-03-01 20:42:08 +0530

    updated UserController

--- api/src/main/java/com/taskmanager/api/controller/UserControllerImpl.java
+++ api/src/main/java/com/taskmanager/api/controller/UserControllerImpl.java
@@ -1,9 +1,12 @@
 package com.taskmanager.api.controller;
 
 import com.taskmanager.api.dto.UserDTO;
-import com.taskmanager.api.entity.UserEntity;
 import com.taskmanager.api.model.User;
+import com.taskmanager.api.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +16,17 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/users")
 public class UserControllerImpl implements UserController {
+
+    private final UserService userService;
+
+    public UserControllerImpl(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
-    public ResponseEntity<UserDTO> createUser(@RequestBody User user) {
-        return null;
+    @PostMapping("/create")
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
     }
 
     @Override
