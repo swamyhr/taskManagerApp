45122cdd130dd53847dfda4b0e2930849f45a608
swamyhr <swamyrangannanayaka@gmail.com>
2026-03-01 20:41:53 +0530

    updated UserServiceImpl to handle duplicate Email validation

--- api/src/main/java/com/taskmanager/api/service/UserServiceImpl.java
+++ api/src/main/java/com/taskmanager/api/service/UserServiceImpl.java
@@ -2,8 +2,9 @@ package com.taskmanager.api.service;
 
 import com.taskmanager.api.dto.UserDTO;
 import com.taskmanager.api.entity.UserEntity;
+import com.taskmanager.api.exception.custom.DuplicateEmailException;
 import com.taskmanager.api.mapper.UserMapper;
-import com.taskmanager.api.model.Users;
+import com.taskmanager.api.model.User;
 import com.taskmanager.api.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -25,11 +26,15 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDTO createUser(Users user) {
+    public UserDTO createUser(User user) {
+        String email = user.getEmail();
+        if(email != null && userRepository.existsByEmail(email)) {
+            throw new DuplicateEmailException("Email already exists");
+        }
+
         UserEntity userEntity = userMapper.toEntity(user);
 
         userEntity.setPassword(passwordEncoder.encode(user.getPassword()));
-
         UserEntity savedUserEntity = userRepository.save(userEntity);
 
         return userMapper.toUserDTO(savedUserEntity);
